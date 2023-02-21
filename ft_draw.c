5443eb73877eb40365ffea0ba952d6ab3bc759ef
Giovanni Mattei <gmattei@c2r3p9.42firenze.it>
2023-02-21 20:23:58 +0100

    make push update

--- ft_draw.c
+++ ft_draw.c
@@ -29,9 +29,28 @@ void	my_pixel_put(t_img *img, int x, int y, int color)
 
 int draw(t_data *data)
 {
+	int i;
+	int j;
+
+	i = 0;
+	j = 0;
 	if (data->win_ptr == NULL)
 		return (1);
-	my_pixel_put(&data->img, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2, RED_PIXEL);
+	while(j < WINDOW_HEIGHT)
+	{
+		i = 0;
+		while(i < data->img.line_len)
+		{
+			if(j * i % 2 == 0)
+				my_pixel_put(&data->img, i, j, GREEN_PIXEL);
+			else
+				my_pixel_put(&data->img, i, j, RED_PIXEL);
+			i++;
+		}
+		j++;
+	}
+	// x = ((px - 100) / 100) * 2;
+	// y = ((100 - py) / 100) * 2;
 	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img, 0, 0);
 	return (0);
 }
