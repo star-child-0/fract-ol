0e8d1f0d1fce18b2780f00691f1b8890e28428f0
Scelfo42 <rimscelfo.christian@gmail.com>
2023-02-27 17:44:29 +0100

    make push update

--- src/fractol.h
+++ src/fractol.h
@@ -6,6 +6,9 @@
 /*   By: anvannin <anvannin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 17:02:42 by cscelfo           #+#    #+#             */
+/*   Updated: 2023/02/27 17:41:17 by cscelfo          ###   ########.fr       */
+/*   Updated: 2023/02/20 19:41:08 by anvannin         ###   ########.fr       */
+/*   Updated: 2023/02/25 18:25:03 by anvannin         ###   ########.fr       */
 /*   Updated: 2023/02/27 17:26:01 by cscelfo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
@@ -23,10 +26,10 @@
 
 //# define WINDOW_WIDTH 1920
 //# define WINDOW_HEIGHT 1080
-# define WINDOW_WIDTH 1000
-# define WINDOW_HEIGHT 1000
+# define WINDOW_WIDTH 500
+# define WINDOW_HEIGHT 800
 # define MLX_ERROR -1
-# define MAX_ITER 10
+# define MAX_ITER 30
 
 typedef struct s_img
 {
@@ -42,10 +45,10 @@ typedef struct s_data
 	void	*mlx_ptr;
 	void	*win_ptr;
 	t_img	img;
-	int		x;
-	int		y;
-	int		color;
-	float	zoom;
+	int x;
+	int y;
+	int color;
+	float zoom;
 }	t_data;
 
 typedef struct s_float_coords
@@ -62,10 +65,10 @@ int		hook_handler(int keycode, t_data *data);
 int		hook_close_handler(t_data *data);
 //int	ft_handle_nothing(void *data);
 int		mouse_hook_handler(int button, t_data *data);
-int		pass(float x, float y);
-int		ft_draw_pixels(t_data *data);
-int		key_hook_handler(int keycode, t_data *data);
-void	my_pixel_put(t_img *img, int x, int y, int color);
+int	pass(float x, float y);
+int	ft_draw_pixels(t_data *data);
+void	key_hook_handler(int keycode, t_data *data);
+void	my_pixel_put(t_img_fra *img, int x, int y, int color);
 void	draw(t_data *data);
 int		draw_handle(t_data *data);
 int		ft_mandelbrot_pass(t_coords *coord);
