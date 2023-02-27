a3a532eb8ac7bcf5292d479b2412d7c81011805b
Scelfo42 <rimscelfo.christian@gmail.com>
2023-02-27 17:45:57 +0100

    make push update

--- src/fractol.h
+++ src/fractol.h
@@ -6,16 +6,10 @@
 /*   By: anvannin <anvannin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/10 17:02:42 by cscelfo           #+#    #+#             */
-<<<<<<< HEAD
-<<<<<<< HEAD:fractol.h
-/*   Updated: 2023/02/22 18:10:00 by cscelfo          ###   ########.fr       */
+/*   Updated: 2023/02/27 17:45:42 by cscelfo          ###   ########.fr       */
 /*   Updated: 2023/02/20 19:41:08 by anvannin         ###   ########.fr       */
-=======
 /*   Updated: 2023/02/25 18:25:03 by anvannin         ###   ########.fr       */
->>>>>>> main:src/fractol.h
-=======
 /*   Updated: 2023/02/27 17:26:01 by cscelfo          ###   ########.fr       */
->>>>>>> main
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,32 +31,24 @@
 # define MLX_ERROR -1
 # define MAX_ITER 30
 
-typedef struct s_img_fra
+typedef struct s_img
 {
 	void	*mlx_img;
 	char	*addr;
 	int		bpp;
 	int		line_len;
 	int		endian;
-}	t_img_fra;
+}	t_img;
 
 typedef struct s_data
 {
 	void	*mlx_ptr;
 	void	*win_ptr;
-<<<<<<< HEAD
 	t_img	img;
 	int		x;
 	int		y;
 	int		color;
 	float	zoom;
-=======
-	t_img_fra	img;
-	int x;
-	int y;
-	int color;
-	float zoom;
->>>>>>> origin/gmattei
 }	t_data;
 
 typedef struct s_float_coords
@@ -78,21 +64,14 @@ typedef struct s_float_coords
 int		hook_handler(int keycode, t_data *data);
 int		hook_close_handler(t_data *data);
 //int	ft_handle_nothing(void *data);
-<<<<<<< HEAD
 int		mouse_hook_handler(int button, t_data *data);
 int		pass(float x, float y);
 int		ft_draw_pixels(t_data *data);
 int		key_hook_handler(int keycode, t_data *data);
 void	my_pixel_put(t_img *img, int x, int y, int color);
-=======
-void mouse_hook_handler(int button, t_data *data);
 int	pass(float x, float y);
 int	ft_draw_pixels(t_data *data);
 void	key_hook_handler(int keycode, t_data *data);
 void	my_pixel_put(t_img_fra *img, int x, int y, int color);
->>>>>>> origin/gmattei
-void	draw(t_data *data);
-int		draw_handle(t_data *data);
-int		ft_mandelbrot_pass(t_coords *coord);
 
 #endif
