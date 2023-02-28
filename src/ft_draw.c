951e911832df2bbb7233c2b8ad31d79b5bc19317
gmattei <gmattei85@gmail.com>
2023-02-28 16:54:59 +0100

    make push update

--- src/ft_draw.c
+++ src/ft_draw.c
@@ -39,7 +39,7 @@ void	draw(t_data *data)
 	t_coords	coord;
 	int			iterations;
 	int			small_side;
-	data->img.mlx_img = mlx_new_image(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	//data->img.mlx_img = mlx_new_image(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
 	iterations = 0;
 	small_side = WINDOW_HEIGHT;
 	if (WINDOW_WIDTH < WINDOW_HEIGHT)
@@ -53,14 +53,14 @@ void	draw(t_data *data)
 			coord.y = (((small_side / data->zoom) - (coord.py + data->add_py)) / (small_side / data->zoom)) * data->zoom;
 			iterations = ft_mandelbrot_pass(&coord);
 			if (iterations < MAX_ITER)
-				my_pixel_put(&data->img, coord.px, coord.py,
-					data->color + iterations * 5);
+				my_pixel_put(data->img, coord.px, coord.py,
+					data->color + pow(iterations, 5.5));
 			coord.py++;
 		}
 		coord.px++;
 	}
 	//ft_printf("\n\npx: %d, py: %d, coord->x: %d, coord->y: %d, data->x: %d, data->y:%d\n\n", coord.px, coord.py, coord.x, coord.y, data->add_px, data->add_py);
-	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img.mlx_img,
+	mlx_put_image_to_window(data->mlx_ptr, data->win_ptr, data->img->mlx_img,
 		0, 0);
 	window_labels(data);
 }
@@ -76,7 +76,7 @@ int	draw_handle(t_data *data)
 	data->add_px = 0;
 	data->add_py = 0;
 	data->zoom = 2;
-	data->color = 0x0000ff;
+	data->color = 0x080808;
 	draw(data);
 	return (0);
 }
--- src/ft_main.c
+++ src/ft_main.c
@@ -14,27 +14,30 @@
 
 int	main(void)
 {
-	t_data	data;
+	t_data	*data;
 
-	data.mlx_ptr = mlx_init();
-	if (data.mlx_ptr == NULL)
+	data = NULL;
+	data = malloc(sizeof(t_data));
+	data->mlx_ptr = mlx_init();
+	if (data->mlx_ptr == NULL)
 		return (MLX_ERROR);
-	data.win_ptr = mlx_new_window(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
+	data->win_ptr = mlx_new_window(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
 			"fract-ol");
-	if (data.win_ptr == NULL)
+	if (data->win_ptr == NULL)
 	{
-		free(data.win_ptr);
+		free(data->win_ptr);
 		return (MLX_ERROR);
 	}
-	data.img.mlx_img = mlx_new_image(data.mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
-	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
-			&data.img.line_len, &data.img.endian);
-	mlx_destroy_image(data.mlx_ptr, data.img.mlx_img);
-	mlx_key_hook(data.win_ptr, &hook_handler, &data);
-	mlx_hook(data.win_ptr, 17, 1L<<17, &hook_close_handler, &data);
-	draw_handle(&data);
-	mlx_loop(data.mlx_ptr);
-	mlx_destroy_display(data.mlx_ptr);
-	free(data.mlx_ptr);
+	data->img = malloc(sizeof(t_img));
+	data->img->mlx_img = mlx_new_image(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT);
+	data->img->addr = mlx_get_data_addr(data->img->mlx_img, &data->img->bpp,
+			&data->img->line_len, &data->img->endian);
+	//mlx_destroy_image(data->mlx_ptr, data->img->mlx_img);
+	mlx_key_hook(data->win_ptr, &hook_handler, &data);
+	mlx_hook(data->win_ptr, 17, 1L<<17, &hook_close_handler, &data);
+	draw_handle(data);
+	mlx_loop(data->mlx_ptr);
+	mlx_destroy_display(data->mlx_ptr);
+	free(data->mlx_ptr);
 	return (0);
 }
