ca5533dfa7cd14159113dcfc8421872adead951c
gmattei <gmattei85@gmail.com>
2023-03-01 16:29:21 +0100

    make push update

--- src/equation.c
+++ src/equation.c
@@ -18,7 +18,7 @@ void my_mlx_pixel_put(t_img *img, int x, int y, int color)
 
     dst = img->addr + (y * img->line_len + x * (img->bpp / 8));
     ft_printf("---");
-    *(unsigned int*)dst = color;
+    *(int*)dst = color;
 }
 
 int equation(t_data *data)
