199e2297fb0e6e43f45e82cb5e91b9937803e791
gmattei <gmattei85@gmail.com>
2023-03-07 11:26:10 +0100

    scroll zoom, restart with r

--- src/hooks.c
+++ src/hooks.c
@@ -14,12 +14,13 @@
 
 int	mouse_handler(int keycode, int x, int y, t_data *data)
 {
+	mlx_mouse_get_pos(data->mlx_ptr, data->win_ptr, &data->mouse_x, &data->mouse_y);
 	x = data->mouse_x;
 	y = data->mouse_y;
 	if (keycode == 4)
 	{
-		data->move_x += (data->mouse_x - data->small_side / 2) / 2;
-		data->move_y += (data->mouse_y - data->small_side / 2) / 2;
+		data->move_x += data->mouse_x;
+		data->move_y += data->mouse_y;
 		data->x_max /= 1.2;
 		data->x_min /= 1.2;
 		data->y_max /= 1.2;
@@ -28,8 +29,8 @@ int	mouse_handler(int keycode, int x, int y, t_data *data)
 	}
 	else if (keycode == 5)
 	{
-		data->move_x -= (data->mouse_x - data->small_side / 2) / 2;
-		data->move_y -= (data->mouse_y - data->small_side / 2) / 2;
+		data->move_x -= data->mouse_x;
+		data->move_y -= data->mouse_y;
 		data->x_max *= 1.2;
 		data->x_min *= 1.2;
 		data->y_max *= 1.2;
@@ -44,7 +45,6 @@ int	mouse_handler(int keycode, int x, int y, t_data *data)
 
 int	key_handler(int keycode, t_data *data)
 {
-	mlx_mouse_get_pos(data->mlx_ptr, data->win_ptr, &data->mouse_x, &data->mouse_y);
 	if (keycode == XK_Escape)
 	{
 		end_fractal(data);
@@ -60,6 +60,16 @@ int	key_handler(int keycode, t_data *data)
 		data->move_x += WIN_WIDTH / 20;
 	else if (keycode == XK_c)
 		data->color += 0x080808;
+	else if (keycode == XK_r)
+	{
+		data->move_x = 0;
+		data->move_y = 0;
+		data->x_max = 2;
+		data->x_min = -2;
+		data->y_max = 2;
+		data->y_min = -2;
+		data->coord.max_iter = MAX_ITER;
+	}
 	else
 		return (0);
 	draw(data);
